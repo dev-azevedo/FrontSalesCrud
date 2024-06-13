8c3ba4fdded6f682074043be841b524371802698
Jhonatan Azevedo <dev.azevedo@outlook.com>
2024-06-12 23:29:50 -0300

    CHANGE: option menu

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,4 +4,20 @@ const api = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
 
+
+export function setupInterceptors(router) {
+  api.interceptors.response.use(
+    response => {
+      return response;
+    },
+    error => {
+      if (error.response && error.response.status === 401) {
+        router.push("/inicio");
+      }
+      return Promise.reject(error);
+    }
+  );
+}
+
+
 export default api;
